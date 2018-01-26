34767b08c6df00d3e4c01ec1fb14a575e067e543
Weikang Hu <Weikang_hu90@hotmail.com>
2018-01-26 23:29:08 +0100

    Create placeholder for questions. Added timer. Spawn 3 items per player.

--- src/sprites/Ball.js
+++ src/sprites/Ball.js
@@ -1,1 +1,52 @@
-import Phaser from 'phaser'import Sprite from '../services/Sprite';const dragCoefficient = 6000;const amplitude = 20;export default class Ball extends Sprite {  constructor({ asset, x, y, frame, anchorX = 0, anchorY = 0, inputEnabled = true }) {    super({ asset, x, y, frame, anchorX, anchorY, inputEnabled});    game.physics.arcade.enable(this);    this.body.collideWorldBounds = true;    this.body.bounce.set(0.9);    this.body.setCircle(this.width / 2);    this.body.drag = new Phaser.Point(4000, 4000)    this.inputEnabled = true;    this.input.enableDrag();    this.scale.setTo(2, 2);    this.positionArray = [      {x: 0, y: 0},      {x: 0, y: 0}    ];    this.events.onDragStop.add(this.stopDrag.bind(this));  }  update () {    this.positionArray.shift();    this.positionArray.push({ x: this.x, y: this.y });    this.body.drag = this.calculateVelocityAbsolute().normalize().multiply(dragCoefficient, dragCoefficient);  }  stopDrag () {    this.body.velocity = this.calculateVelocity();  }  calculateVelocity () {    return new Phaser.Point((this.positionArray[1].x - this.positionArray[0].x) * amplitude, (this.positionArray[1].y - this.positionArray[0].y)*amplitude);  }  calculateVelocityAbsolute () {    return new Phaser.Point(Math.abs((this.positionArray[1].x - this.positionArray[0].x) * amplitude), Math.abs((this.positionArray[1].y - this.positionArray[0].y)*amplitude));  }  render () {    game.debug.body(this);  }}
+import Phaser from 'phaser'
+import Sprite from '../services/Sprite';
+
+const dragCoefficient = 6000;
+const amplitude = 20;
+
+export default class Ball extends Sprite {
+  constructor({ asset, x, y, frame, anchorX = 0, anchorY = 0, inputEnabled = true }) {
+    super({ asset, x, y, frame, anchorX, anchorY, inputEnabled});
+
+    game.physics.arcade.enable(this);
+    this.body.collideWorldBounds = true;
+    this.body.bounce.set(0.9);
+    this.body.setCircle(this.width / 2);
+    this.body.drag = new Phaser.Point(4000, 4000)
+    this.inputEnabled = true;
+    this.input.enableDrag();
+    this.positionArray = [
+      {x: 0, y: 0},
+      {x: 0, y: 0}
+    ];
+
+    this.angle = Math.random() * 360;
+
+    this.events.onDragStop.add(this.stopDrag.bind(this));
+    this.scale.setTo(0);
+    game.add.tween(this.scale).to({ x: 1, y: 1 }, 400, Phaser.Easing.Bounce.Out, true);
+  }
+
+  update () {
+    this.positionArray.shift();
+    this.positionArray.push({ x: this.x, y: this.y });
+
+    this.body.drag = this.calculateVelocityAbsolute().normalize().multiply(dragCoefficient, dragCoefficient);
+  }
+
+  stopDrag () {
+    this.body.velocity = this.calculateVelocity();
+  }
+
+  calculateVelocity () {
+    return new Phaser.Point((this.positionArray[1].x - this.positionArray[0].x) * amplitude, (this.positionArray[1].y - this.positionArray[0].y)*amplitude);
+  }
+
+  calculateVelocityAbsolute () {
+    return new Phaser.Point(Math.abs((this.positionArray[1].x - this.positionArray[0].x) * amplitude), Math.abs((this.positionArray[1].y - this.positionArray[0].y)*amplitude));
+  }
+
+  render () {
+    game.debug.body(this);
+  }
+}
--- src/states/Game.js
+++ src/states/Game.js
@@ -1,30 +1,25 @@
 /* globals __DEV__ */
 import Phaser from 'phaser';
-import Mushroom from '../sprites/Mushroom';
 import AllBalls from '../sprites/AllBalls';
+import Questions from '../sprites/Questions';
+import CountDown from '../sprites/CountDown';
+
 
 export default class extends Phaser.State {
   init () {
     game.physics.startSystem(Phaser.Physics.arcade);
+    this.game.startCountDown = new Phaser.Signal();
   }
   preload () {}
 
   create () {
     this.allBalls = new AllBalls(game);
-
-
-    this.mushroom = new Mushroom({
-      game: game,
-      asset: 'mushroom',
-      x: 100,
-      y: 100,
-    });
-
+    this.questions = new Questions(game, 'Placeholder?');
+    this.countDown = new CountDown(game);
   }
 
   render () {
     if (__DEV__) {
-      this.allBalls.render()
     }
   }
 }
