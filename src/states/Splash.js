ca8123f5fb2597f889780d8eb580d2c3088ffa26
Yosri Jerbi <yosri.jerbi@hotmail.com>
2018-01-27 20:17:36 +0100

    shts poppin'

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -48,6 +48,7 @@ export default class extends Phaser.State {
     this.load.image('play-buttonbg', 'assets/images/play-buttonbg.png');
     this.load.image('cross', 'assets/images/cross.png');
     this.load.image('star', 'assets/images/star.png');
+    this.load.image('plop', 'assets/images/plop-particle.png');
   }
 
   create () {
