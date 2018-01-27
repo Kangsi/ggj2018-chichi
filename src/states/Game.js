1b7156be60e3acfcc2fc5bc304221897b2ea27e6
Yosri Jerbi <yosri.jerbi@hotmail.com>
2018-01-27 01:57:24 +0100

    added gameTimer

--- src/states/Game.js
+++ src/states/Game.js
@@ -3,12 +3,20 @@ import Phaser from 'phaser';
 import AllBalls from '../sprites/AllBalls';
 import Questions from '../sprites/Questions';
 import CountDown from '../sprites/CountDown';
-
+import GameTimer from '../sprites/GameTimer';
 
 export default class extends Phaser.State {
   init () {
     game.physics.startSystem(Phaser.Physics.arcade);
     this.game.startCountDown = new Phaser.Signal();
+    this.game.startGameTimer = new Phaser.Signal();
+    this.game.endRound = new Phaser.Signal();
+
+    this.game.endRound.add(() => {
+      setTimeout(() => {
+        this.game.state.start('Game');
+      }, 500)
+    });
   }
   preload () {}
 
@@ -16,6 +24,7 @@ export default class extends Phaser.State {
     this.allBalls = new AllBalls(game);
     this.questions = new Questions(game, 'Placeholder?');
     this.countDown = new CountDown(game);
+    this.gameTimer = new GameTimer(game, 5);
   }
 
   render () {
